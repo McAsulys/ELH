f6b45cb2bcb156573eae7547faa20c0d72b9d96e
McAsulys <benedict.dourlens@gmail.com>
2018-02-21 16:02:32 +0100

    integretion of font.

--- client/main.js
+++ client/main.js
@@ -7,11 +7,13 @@ import City from "./City.js";
 
 ReactDOM.render(
     <div>
+      <body>
         <Router history={hashHistory}>
             <Route path="/" component={Home}/>
             <Route path="/city/:id" component={City} />
             <Route path="*" component={() => <p>Page Not Found</p>}/>
         </Router>
+      </body>
     </div>
     ,
     document.getElementById('root')
